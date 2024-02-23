4ad92264fbecd98139c067f77d2db2f29e2d4e6b
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook-Pro-2.local>
2024-02-22 22:36:14 -0600

    fixing hero spacing

--- app/ui/home/cta-banner.tsx
+++ app/ui/home/cta-banner.tsx
@@ -3,7 +3,7 @@ import Button from 'components/button';
 export default function CtaBanner() {
   return (
     <section className="lg:mt-42 mt-48 flex h-auto flex-col items-center bg-white px-4 py-[84px] sm:px-8 md:px-12 lg:px-16">
-      <h2 className="mb-6 text-2xl text-gray-900 sm:text-3xl md:text-4xl lg:text-5xl">
+      <h2 className="mb-2 text-2xl text-gray-900 sm:text-3xl md:text-4xl lg:mb-6 lg:text-5xl">
         Not sure where to begin?
       </h2>
       <p className="mb-10 w-4/5 text-center text-lg text-gray-900">
