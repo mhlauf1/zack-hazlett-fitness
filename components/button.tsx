9e255f9aa063b2773b8b19bf8f6ed0167213b1b2
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook-Pro-2.local>
2024-02-22 20:24:31 -0600

    fixing hero mobile side spacing

--- components/button.tsx
+++ components/button.tsx
@@ -6,7 +6,7 @@ import styles from '../app/ui/styles.module.css';
 const Button = ({ children, href }: { children: ReactNode; href: string }) => {
   return (
     <Link className={`${styles.buttonContainer} flex w-full lg:w-auto`} href={href}>
-      <button className="text-md w-full rounded-full border-none bg-[#008FED] px-12 py-3 font-medium lg:w-auto lg:py-4 lg:text-lg">
+      <button className="text-md w-full rounded-full border-none bg-[#008FED] px-10 py-3 font-medium lg:w-auto lg:py-4 lg:text-lg">
         {children}
       </button>
       <div className="flex h-12 w-14 items-center justify-center rounded-full bg-[#008FED] lg:h-16 lg:w-16">
