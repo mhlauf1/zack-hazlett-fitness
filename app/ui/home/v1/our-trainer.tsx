bca2b509a720bc26af2774526ec1eed0bf130c9c
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook-Pro-2.local>
2024-02-27 08:40:47 -0600

    fixing button import

--- app/ui/home/v1/our-trainer.tsx
+++ app/ui/home/v1/our-trainer.tsx
@@ -1,5 +1,5 @@
 import AnimateWrapper from 'components/animated/animate-wrapper';
-import Button from 'components/buttons';
+import { Button } from 'components/buttons';
 import SectionLabel from 'components/section-label';
 import Image from 'next/image';
 
