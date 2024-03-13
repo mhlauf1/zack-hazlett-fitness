d607dbf886ebeb3a42d86c8d575281f697cce01f
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook-Pro-2.local>
2024-03-13 11:49:30 -0500

    changing programs default to be latest arrivals

--- lib/constants.ts
+++ lib/constants.ts
@@ -6,16 +6,16 @@ export type SortFilterItemType = {
 };
 
 export const defaultSort: SortFilterItemType = {
-  title: 'Relevance',
+  title: 'Latest arrivals',
   slug: null,
-  sortKey: 'RELEVANCE',
-  reverse: false
+  sortKey: 'CREATED_AT',
+  reverse: true
 };
 
 export const sorting: SortFilterItemType[] = [
   defaultSort,
   { title: 'Trending', slug: 'trending-desc', sortKey: 'BEST_SELLING', reverse: false }, // asc
-  { title: 'Latest arrivals', slug: 'latest-desc', sortKey: 'CREATED_AT', reverse: true },
+  { title: 'Relevance', slug: 'relevance-asc', sortKey: 'RELEVANCE', reverse: true },
   { title: 'Price: Low to high', slug: 'price-asc', sortKey: 'PRICE', reverse: false }, // asc
   { title: 'Price: High to low', slug: 'price-desc', sortKey: 'PRICE', reverse: true }
 ];
