31314a27bb80de6252b9db9a37637543a411f195
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook-Pro-2.local>
2024-04-02 11:41:54 -0500

    updating copy

--- app/ui/home/Featured.tsx
+++ app/ui/home/Featured.tsx
@@ -13,12 +13,10 @@ export default async function Featured() {
         >
           Available Programs
         </h2>
-        <p className="lg mt-2 w-full text-xl leading-8 text-gray-600 sm:w-4/5  lg:w-1/2">
-          Embark on a journey to peak fitness with Zack Hazlett Fitness. From intensive weight
-          training to nourishing wellness practices. Find your strength and vitality for everyday
-          health.
+        <p className="lg w-full text-xl leading-8 text-gray-600 sm:w-4/5  lg:w-1/2">
+          Check out our top fitness programs, tailored to kickstart your journey.
         </p>
-        <div className="relative mt-12 grid grid-cols-1 gap-y-10 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-0 lg:gap-x-8">
+        <div className="relative mt-8 grid grid-cols-1 gap-y-10 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-0 lg:gap-x-8">
           {featuredProgram
             ? featuredProgram.map((program) => <HomeProgram featuredProgram={program} />)
             : null}
