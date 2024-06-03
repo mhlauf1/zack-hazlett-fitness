f35d92307dd35de0d578af3a9da4e20c0691a254
Michael Laufersweiler <michael.laufersweiler@lulafit.com>
2024-06-03 09:25:07 -0500

    looking for production stuff

--- lib/contentful.ts
+++ lib/contentful.ts
@@ -23,7 +23,7 @@ const ABOUT_PHOTOS_GRAPHQL_FIELDS = `
   }
 `;
 
-async function fetchGraphQL(query: any, preview = false) {
+async function fetchGraphQL(query: string, preview = false) {
   const response = await fetch(
     `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
     {
@@ -36,11 +36,18 @@ async function fetchGraphQL(query: any, preview = false) {
             : process.env.CONTENTFUL_ACCESS_TOKEN
         }`
       },
-      body: JSON.stringify({ query }),
-      next: { tags: ['heroSection', 'aboutPhotos'] }
+      body: JSON.stringify({ query })
     }
   );
+
   const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(
+      `Error fetching data: ${data.errors.map((err: any) => err.message).join(', ')}`
+    );
+  }
+
   return data;
 }
 
@@ -55,7 +62,7 @@ function extractAboutPhotosEntries(fetchResponse: any) {
 export async function getHeroSection(isDraftMode = false) {
   const heroSection = await fetchGraphQL(
     `query {
-      heroSectionCollection(limit: 1, preview: ${isDraftMode ? 'true' : 'false'}) {
+      heroSectionCollection(limit: 1, preview: ${isDraftMode}) {
         items {
           ${HERO_GRAPHQL_FIELDS}
         }
@@ -63,19 +70,31 @@ export async function getHeroSection(isDraftMode = false) {
     }`,
     isDraftMode
   );
-  return extractHeroEntries(heroSection)[0] || null;
+
+  const heroEntries = extractHeroEntries(heroSection);
+  if (heroEntries.length === 0) {
+    console.warn('No hero section data found');
+  }
+
+  return heroEntries[0] || null;
 }
 
 export async function getAboutPhotos(isDraftMode = false) {
   const aboutPhotos = await fetchGraphQL(
     `query {
-    aboutPhotosCollection(limit: 1, preview: ${isDraftMode ? 'true' : 'false'}) {
-      items {
-        ${ABOUT_PHOTOS_GRAPHQL_FIELDS}
+      aboutPhotosCollection(limit: 1, preview: ${isDraftMode}) {
+        items {
+          ${ABOUT_PHOTOS_GRAPHQL_FIELDS}
+        }
       }
-    }
-  }`,
+    }`,
     isDraftMode
   );
-  return extractAboutPhotosEntries(aboutPhotos)[0] || null;
+
+  const aboutPhotoEntries = extractAboutPhotosEntries(aboutPhotos);
+  if (aboutPhotoEntries.length === 0) {
+    console.warn('No about photos data found');
+  }
+
+  return aboutPhotoEntries[0] || null;
 }
